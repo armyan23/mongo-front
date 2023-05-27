0a76417911688edb6f7c7c1e63d684dcf673e37c
armyan23 <arm.davtyan1029@gmail.com>
2023-05-28 02:23:49 +0400

    Add padding

--- pages/people/index.js
+++ pages/people/index.js
@@ -15,7 +15,7 @@ const People = () => {
 
   return (
     <Box>
-      <Grid container spacing={4}>
+      <Grid container spacing={4} p={3}>
         {people.map((person) => {
           return (
             <Grid item xs={12} sm={4} lg={3} xl={2} key={person["_id"]}>
